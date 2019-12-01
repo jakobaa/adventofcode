19aee540b09c98a07a2b467dd9e7719747f930be
Jakob Aasa <jakob@aasa.xyz>
2019-12-01 12:46:12 +0100

    Finish day 1

---
+++ 2019/1/day1.hs
@@ -0,0 +1,16 @@
+import Data.Maybe
+import Text.Read
+type Mass = Int
+type Fuel = Int
+
+calcFuel :: Mass -> Fuel
+calcFuel m = (m `div` 3) - 2
+
+main :: IO ()
+main = readFile "input.txt" >>=
+          print . foldr ((+) . fuelForFuel . calcFuel) 0 . map read . lines
+
+fuelForFuel :: Fuel -> Fuel
+fuelForFuel fuel
+  | fuel > 0  = fuel + fuelForFuel (calcFuel fuel)
+  | otherwise = 0
