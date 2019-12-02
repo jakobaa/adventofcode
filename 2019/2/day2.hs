fec60fe5145386bcee7b1902b7d984fa74e3f56a
Jakob Aasa <jakob@aasa.xyz>
2019-12-02 23:02:33 +0100

    add some code for mutable array version

--- 2019/2/day2.hs
+++ 2019/2/day2.hs
@@ -1,11 +1,18 @@
+
 import Data.List.Split
 import Data.Array.ST
 import Control.Monad.ST
-import Control.Monad.Writer
+import Control.Monad
 
-type Arg = Int
+
 type Index = Int
 type List = [Int]
+type IntCode = Int
+
+               --       s index content
+type Array s = STUArray s Index IntCode
+
+data Operation = Add | Mul
 
 parseInput :: String -> IO List
 parseInput file = readFile file >>= return . map read . endByOneOf ",\n"
@@ -19,6 +26,46 @@ main = do input <- parseInput "input.txt"
           print . head $ filter (\a -> head a == 19690720) allInputs
 
 
+--fillArray :: STUArray s Int IntCode -> [Int] -> ST s (STUArray s Int IntCode)
+--fillArray arr list = 
+
+makeArray :: [Int] -> ST s (Array s)
+makeArray list = do arr <- newListArray (0,length list - 1) list :: ST s (Array s)
+                    return arr
+
+readIndices :: [Index] -> Array s -> ST s [Int]
+readIndices xs arr = forM xs (\x -> getOne x arr)
+
+getOne :: Int -> Array s -> ST s IntCode
+getOne i ass = do a <- readArray ass i
+                  return a
+
+doOp :: Operation -> [Index] -> Array s -> ST s ()
+doOp Add (i1:i2:out:[]) arr = do x <- readArray arr i1
+                                 y <- readArray arr i2
+                                 writeArray arr out (x+y)
+doOp Mul (i1:i2:out:[]) arr = undefined
+
+runIntCodes :: Array s -> Index -> ST s (Array s)
+runIntCodes arr i = undefined
+
+runStateThing :: [Int] -> ST s ()
+runStateThing input = do arr <- makeArray input
+                         return ()
+
+newMain :: IO ()
+newMain = do input <- parseInput "input.txt"
+             let arst = runST $ runStateThing input
+             return ()
+--             print . head $ filter (\a -> head a == 19690720) allInputs
+
+
+doStuffArray intList = do arr <- makeArray intList
+                          a <- readArray arr $ 3
+                          doOp Add [1,2,3,4] arr
+                          b <- readArray arr $ 3
+                          c <- readIndices [4,5,6] arr
+                          return (a,b,c)
 
 doInput :: Index -> List ->  List
 doInput start list = case list!!start of
@@ -31,6 +78,6 @@ doInput start list = case list!!start of
     arg2 = list!!in2
     (op:in1:in2:out:xs) = take 4 (drop start list)
 
-replace :: Arg -> Index -> List -> List
+replace :: Int -> Index -> List -> List
 replace x i list = let (li, st) = splitAt i list
                  in li ++ x:(drop 1 st)
